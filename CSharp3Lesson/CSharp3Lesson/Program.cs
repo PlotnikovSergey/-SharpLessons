19234e801e92921c7ecbe9d7125b2cb510c5913d
Sergey <walkeroncloud@mail.ru>
2018-12-21 01:02:55 +0700

    Для ветки lesson3 оставили файлы относящиеся только к lesson3

---
+++ CSharp3Lesson/CSharp3Lesson/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp3Lesson
+{
+    partial class Program
+    {
+            static void Main(string[] args)
+            {
+                Menu();
+            }
+
+            static void Menu()
+            {
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Урок 3. Плотников Сергей.");
+                    Console.WriteLine("Нажмите соответствующую клавишу для вывода решения или выхода из программы.\n");
+                    Console.WriteLine("1 - Задание 1\n2 - Задание 2\n3 - Задание 3\n0 - Выход");
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.D1:
+                        case ConsoleKey.NumPad1:
+                            Program.Task1();
+                            break;
+                        case ConsoleKey.D2:
+                        case ConsoleKey.NumPad2:
+                            Program.Task2();
+                            break;
+                        case ConsoleKey.D3:
+                        case ConsoleKey.NumPad3:
+                            Program.Task3();
+                            break;
+                        case ConsoleKey.D0:
+                        case ConsoleKey.NumPad0:
+                        case ConsoleKey.Escape:
+                            Console.WriteLine("Программа завершена!");
+                            return;
+                        default:
+                            Console.WriteLine("Нажмите кнопку, согласно подсказке");
+                            break;
+                    }
+                }
+                while (true);
+            }
+    }
+}
---
+++ CSharp3Lesson/CSharp3Lesson/Task3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp3Lesson
+{
+    partial class Program
+    {
+        static void Task3()
+        {
+        }
+    }
+}
